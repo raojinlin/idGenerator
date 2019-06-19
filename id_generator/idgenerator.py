317bf98f7b67cbbc8c6a863f412e4375d0044676
raojinlin <1239015423@qq.com>
2019-06-19 15:12:14 +0800

    fix: id duplicate

--- id_generator/idgenerator.py
+++ id_generator/idgenerator.py
@@ -1,15 +1,16 @@
 from src.client import get_shard
+from time import time as timstamp
 
 
 class IDGenerator:
     """redis 分布式id生成器
-    共64位：0(标志位)|41(时间戳,微秒)|12(shard_id)|10(sequence)
+    共64位：0(标志位)|41(时间戳,毫秒)|10(shard_id)|12(sequence)
     """
     def __init__(self, redis_shard):
         self.REDIS_SHARD_ID = redis_shard
         self.START_TIME = 1138723200000
-        self.SHARD_ID_BITS = 12
-        self.SEQUENCE_BITS = 10
+        self.SHARD_ID_BITS = 10
+        self.SEQUENCE_BITS = 12
         self.TIMESTAMP_BITS = self.SHARD_ID_BITS + self.SEQUENCE_BITS
         self.REDIS_SHARD_ID_KEY = 'py-id-generator-shard-id'
         self.REDIS_SEQUENCE_KEY = 'py-id-generator-sequence'
@@ -24,7 +25,7 @@ class IDGenerator:
 
     def get_time(self):
         time, microsecond = self.redis.time()
-        return time * self.ONE_SECOND_OF_MICRO + microsecond - self.START_TIME
+        return time * self.ONE_SECOND_OF_MICRO + int(microsecond / self.ONE_SECOND_OF_MICRO) - self.START_TIME
 
     def sequence_full(self):
         current_sequence = int(self.redis.get(self.REDIS_SEQUENCE_KEY).decode('utf-8'))
@@ -38,7 +39,7 @@ class IDGenerator:
         return int(redis_shard_id) == self.REDIS_SHARD_ID
 
     def get_id(self):
-        return self.get_ids()[0]
+        return list(self.get_ids())[0]
 
     def get_ids(self, count=1):
         if self.sequence_full():
@@ -48,19 +49,16 @@ class IDGenerator:
         seq_start = seq_end - count
         time = self.get_time()
 
-        ids = []
         while seq_start < seq_end:
             time_shifted = time << self.TIMESTAMP_BITS
             shard_shifted = self.REDIS_SHARD_ID << self.SHARD_ID_BITS
             sequence_shifted = seq_start << self.SEQUENCE_BITS
 
-            ids.append(time_shifted | shard_shifted | sequence_shifted)
+            yield (time_shifted | shard_shifted | sequence_shifted)
             seq_start += 1
-
-        return ids
 
 
 if __name__ == '__main__':
-    generator = IDGenerator(1)
-    res = generator.get_ids(10)
-    print(res)
+    generator = IDGenerator(2)
+    print(generator.get_id())
+
